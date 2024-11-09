5b8c5ee92203b0e49e0446c2cfd5e9c4d2a3eb95
dhaval079 <rupaparadhaval1234@gmail.com>
2024-11-09 17:02:39 +0530

    added user location permission

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -46,8 +46,8 @@ app.use(cookieParser());
 //   origin: 'https://LogistiQ-atlan.vercel.app',
   
 const corsOptions = {
-  // origin: ['https://logistiq-atlan.vercel.app'],
-  origin: ['http://localhost:3000'],
+  origin: ['https://logistiq-atlan.vercel.app'],
+  // origin: ['http://localhost:3000'],
   // origin: ['https://logistiq-atlan.vercel.app', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
